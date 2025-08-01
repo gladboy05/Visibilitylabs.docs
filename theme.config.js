979e869359e7681c4f332e8bf4fcbe07a680fc6f
gladboy05 <gladgraphicals@gmail.com>
2025-08-01 10:51:08 +0100

    Add files via upload

---
+++ theme.config.js
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>VisibilityLabs</span>,
+  project: {
+    link: 'https://github.com/YOUR_USERNAME/visibilitylabs-docs',
+  },
+  docsRepositoryBase: 'https://github.com/YOUR_USERNAME/visibilitylabs-docs/blob/main',
+  footer: {
+    text: 'VisibilityLabs © 2025',
+  },
+}
